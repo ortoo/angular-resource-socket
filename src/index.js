08b4ff2acaf2d920745865a30f7be013edc4bb9f
James Sharp <james@ortootech.com>
2017-10-10 21:09:09 +0100

    remove the cachebuster

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import angular from 'angular';
 
 import Socket from './socket.js';
 import RetryInterceptor from './retryInterceptor.js';
-import CacheBuster from './cacheBuster.js';
 
 export default angular.module('or2.socket', [])
   .provider('socketRetryInterceptor', RetryInterceptor)
-  .provider('socketCacheBuster', CacheBuster)
   .provider('socket', Socket)
   .config(function($httpProvider) {
-    $httpProvider.interceptors.push('socketRetryInterceptor', 'socketCacheBuster');
+    $httpProvider.interceptors.push('socketRetryInterceptor');
   });
